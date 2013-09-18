666299e45708a9945ad6b477cd994387f1ed79e0
DamianChyb <DamianChyb@gmail.com>
2013-09-18 22:11:17 +0200

    CHAT UPDATE

--- chat/index.php
+++ chat/index.php
@@ -9,31 +9,44 @@
 		<div id="chat">
 			<div id="display">
 				<?php
-					function writeDisplay($file){ // funkjca drukująca zawartość chat.txt na display -- dokończyć
-						while( !feof($file) ){
-							echo fgets($file);
+					
+					function displayChat(){
+						fgets( $GLOBALS['chatRead'] ); // fwrite pozostawia pustą linie na początku chat.txt
+													  // fgets($chatRead) sprawia że następne użycie fgets będzie wczytywało zapisane linie
+						while( !feof($GLOBALS['chatRead']) ){
+							$nick_and_message = fgets($GLOBALS['chatRead']);
+							$array = explode( ',', $nick_and_message);
+							echo '<p class="message"><span class="nick">' . $array[0] . ' : </span>' . $array[1] . '</p>';
 						}
 					}
 				
-					$message = "";
-					$nick = "";
-					@$message = $_POST["messageInput"]; // by zapobiec błędu gdy nie wprowadzono informacji do formu
-					@$nick = $_POST["nick"];
+					$chatAppend = fopen('chat.txt', 'a'); // otwórz plik z uprawnieniem do dopisywania do pliku
+					$chatRead = fopen('chat.txt', 'r' ); // otwórz plik z uprawnieniem do czytania pliku
 					
-					$chat = fopen("chat.txt", "a");
-						fwrite($char, $nick);
-						fwrite($chat, $message);		
+					if( !empty($_POST["messageInput"]) and !empty($_POST["nick"]) ){ // jeśli dane są wprowadzone do <form>
+						$message = $_POST["messageInput"];
+						$nick = $_POST["nick"];
+
+						fwrite( $chatAppend, "\n" . $nick . ',' . $message );
+						displayChat();
+											    											
+					}else{ // jeśli wprowadzona tylko nazwa użytkownika wyświetl zawartość chat.txt (refresh)
+						displayChat();
+					};
 					
+					fclose($chatRead);
+					fclose($chatAppend);
 					
-					
-					
-					fclose($chat);
 				?>
 			</div>
 			<form method="post" action=" <?php echo $_SERVER['PHP_SELF']; ?>">
-				<p>wiadomość: <input class="input" id="messageInput" name="messageInput" type="text"/></p>
-				<p>nick: <input class="input" id="nick" name="nick" type="text"/></p>
-				<input id="send" type="submit" value="send"/>
+				<p class="inputLine">
+					 <span>Nick: <input class="input" id="nick" name="nick" type="text"/></span>
+					 <span>Wiadomość: <input class="input" id="messageInput" name="messageInput" type="text"/></span>
+				</p>
+				<p class="inputLine">		
+					<input id="send" type="submit" value="Send/Refresh"/>
+				</p>
 			</form>
 		</div>
 	</body>
